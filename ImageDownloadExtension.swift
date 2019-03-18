44741b20494fe754031bcf2b8e537bb228a99d6d
Akash Soni <akashsoni8285@gmail.com>
2019-03-18 18:27:41 +0530

    Create ImageDownloadExtension.swift

---
+++ ImageDownloadExtension.swift
@@ -0,0 +1,16 @@
+extension UIImageView {
+ 
+    func downloadimage(url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+            }
+            }.resume()
+    }
+}
