7fa74ee0af926427487825b5dda61f762fdd486e
Akash Soni <akashsoni8285@gmail.com>
2019-06-09 17:37:22 +0530

    Create GradientView.swift

---
+++ GradientView.swift
@@ -0,0 +1,77 @@
+//  Created by Akash soni on 19/02/19.
+//
+
+import UIKit
+
+@IBDesignable
+class gradient: UIView {
+    @IBInspectable var firstcolor:UIColor = UIColor.clear
+        {
+            didSet
+                {
+                  updatecolor()
+            }
+    }
+    @IBInspectable var Secondcolor:UIColor = UIColor.clear
+        {
+        didSet
+        {
+            updatecolor()
+        }
+    }
+        override class var layerClass: AnyClass
+        {
+        get
+        {
+        return CAGradientLayer.self
+        }
+        }
+    
+    func updatecolor()
+    {
+        let layer = self.layer as? CAGradientLayer
+        layer?.startPoint = CGPoint(x: 0.0, y: 0.5)
+        layer?.endPoint = CGPoint(x: 1.0, y: 0.5)
+        layer?.colors = [firstcolor.cgColor, Secondcolor.cgColor]
+    }
+
+
+}
+class gradienttabbar: UITabBar {
+    @IBInspectable var firstcolor:UIColor = UIColor.clear
+        {
+        didSet
+        {
+            updatecolor()
+        }
+    }
+    @IBInspectable var Secondcolor:UIColor = UIColor.clear
+        {
+        didSet
+        {
+            updatecolor()
+        }
+    }
+    override class var layerClass: AnyClass
+        {
+        get
+        {
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updatecolor()
+    {
+        let layer = self.layer as? CAGradientLayer
+        //layer?.frame = self.tintColor
+        layer?.startPoint = CGPoint(x: 0.0, y: 0.5)
+        layer?.endPoint = CGPoint(x: 1.0, y: 0.5)
+      
+        layer?.colors = [firstcolor.cgColor, Secondcolor.cgColor]
+      
+       // self.layer.addSublayer(layer!)
+        
+    }
+    
+    
+}
