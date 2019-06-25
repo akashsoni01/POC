95e11480e79477a290122a64aa7e6446892e3444
Akash Soni <akashsoni8285@gmail.com>
2019-06-25 16:58:54 +0530

    activity indicator extension in swift 4 ios

---
+++ ActivityIndicator+UIView.swift
@@ -0,0 +1,29 @@
+extension UIView{
+    
+    func activityStartAnimating(activityColor: UIColor, backgroundColor: UIColor) {
+        let backgroundView = UIView()
+        backgroundView.frame = CGRect.init(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)
+        backgroundView.backgroundColor = backgroundColor
+        backgroundView.tag = 475647
+        
+        var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+        activityIndicator = UIActivityIndicatorView(frame: CGRect.init(x: 0, y: 0, width: 50, height: 50))
+        activityIndicator.center = self.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = UIActivityIndicatorView.Style.gray
+        activityIndicator.color = activityColor
+        activityIndicator.startAnimating()
+        self.isUserInteractionEnabled = false
+        
+        backgroundView.addSubview(activityIndicator)
+        
+        self.addSubview(backgroundView)
+    }
+    
+    func activityStopAnimating() {
+        if let background = viewWithTag(475647){
+            background.removeFromSuperview()
+        }
+        self.isUserInteractionEnabled = true
+    }
+}
