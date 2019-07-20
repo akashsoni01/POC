11538172dd1af22b26cd73e79deda23cd4aea72b
Akash Soni <akashsoni8285@gmail.com>
2019-07-21 04:03:21 +0530

    Add files via upload

---
+++ ImagePicker in swfit 4.2/ImagePicker in swfit 4.2/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ImagePicker in swfit 4.2
+//
+//  Created by Akash Soni on 21/07/19.
+//  Copyright © 2019 Akash Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGesture)
+        // Do any additional setup after loading the view.
+    }
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+       // let tappedImage = tapGestureRecognizer.view as! UIImageView
+        // Your action
+        let imagePickerView = UIImagePickerController()
+        imagePickerView.delegate = self
+        imagePickerView.allowsEditing = true
+        imagePickerView.sourceType = .photoLibrary
+        self.present(imagePickerView, animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("cancel tapped")
+        self.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        imageView.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
