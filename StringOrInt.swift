19503c0e0647ac5156a9f15aef564d0acd81c5a4
Akash Soni <akashsoni8285@gmail.com>
2021-12-11 23:12:32 +0530

    String or Int both accept in Decodable in swift
    String or Int both accept in Decodable and Codeable in swift

---
+++ StringOrInt.swift
@@ -0,0 +1,27 @@
+enum StringOrInt: Codable {
+    case integer(Int)
+    case string(String)
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let x = try? container.decode(Int.self) {
+            self = .integer(x)
+            return
+        }
+        if let x = try? container.decode(String.self) {
+            self = .string(x)
+            return
+        }
+        throw DecodingError.typeMismatch(StringOrInt.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for Name"))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .integer(let x):
+            try container.encode(x)
+        case .string(let x):
+            try container.encode(x)
+        }
+    }
+}
