9d798a3ae1bee6c41e5a83eb33a4f8b78a7dc2ca
Akash Soni <akashsoni8285@gmail.com>
2022-10-10 16:38:35 +0530

    Update Api+DataTask.swift

--- Api+DataTask.swift
+++ Api+DataTask.swift
@@ -1,9 +1,16 @@
 public struct Constants{   
     typealias serviceCompletion = (_ data:Data?,_ response: URLResponse?,_ err: Error?) -> Void
       static var baseUrl: String { ............    }
-
   
-  
+}
+extension Data {
+    var prettyJson: String? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+            let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+            let prettyPrintedString = String(data: data, encoding:.utf8) else { return nil }
+        
+        return prettyPrintedString
+    }
 }
 
 /*
